2ed9e02b219e4b82c3dea3979abfaa6c5952beb6
pnarrdgn <pnardogan.1907@gmail.com>
2025-03-14 14:19:02 +0300

    Add files via upload
    tailwind css kullanım örneği.

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web sitesi</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100 font-sans">
+    <header class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto text-center">
+            <nav>
+                <ul class="flex justify-center space-x-8">
+                    <li>
+                        <a href="index.php" class="text-white hover:bg-red-700 px-4 py-2 rounded-lg shadow-lg transition duration-300">Anasayfa</a>
+                    </li>
+                    <li>
+                        <a href="hakkimizda.php" class="text-white hover:bg-red-700 px-4 py-2 rounded-lg shadow-lg transition duration-300">Hakkımızda</a>
+                    </li>
+                    <li>
+                        <a href="iletisim.php" class="text-white hover:bg-red-700 px-4 py-2 rounded-lg shadow-lg transition duration-300">İletişim</a>
+                    </li>
+                    <li>
+                        <a href="blog.php" class="text-white hover:bg-red-700 px-4 py-2 rounded-lg shadow-lg transition duration-300">Blog</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
